d8ea5bc5eca50d284d6088f83c2515a9377f6c31
leegallagher001 <lee.gallagher.writing@outlook.com>
2025-12-30 10:36:49 +0000

    Have got a partially-complete version of the "new entry" part of the journal part working, The "new entry button" works somewhat as intended, although I'm sure there is a better way to do it. The whole page also seems to refresh upon pressing the "cancel entry" button. Will need to find out why that is.

--- main.js
+++ main.js
@@ -7,3 +7,22 @@ const hamburgerMenu = document.querySelector(".hamburger-menu");
 hamburgerIcon.addEventListener("click", function () {
     hamburgerMenu.classList.toggle("dropdown-menu");
 });
+
+/* Journal - New Entry Start Button */
+
+const newEntry = document.querySelector("#journal-new-entry-form");
+const newEntryButton = document.getElementById("journal-new-entry-icon");
+
+newEntryButton.addEventListener("click", function () {
+    newEntry.style.display = "inline-block";
+    newEntryButton.style.display = "none";
+});
+
+/* Journal - Cancel Entry Button */
+
+const cancelEntryButton = document.getElementById("cancel-entry");
+
+cancelEntryButton.addEventListener("click", function () {
+    newEntry.style.display = "none";
+    newEntryButton.style.display = "inline-block";
+});
